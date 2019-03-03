ba2ed59e1f55d40973dc248a7386d0fe54445c4c
11235andrew <sunts_andrew@mail.ru>
2019-03-03 14:38:27 +0300

    Find false negative calls 4.

--- pos_neg.py
+++ pos_neg.py
@@ -12,7 +12,11 @@ def find_false_negative(vcf_file_name,  f_negative_file_name):
     calls = []
     count = 0
     for record in vcf_reader:
-        calls.append(record.__dict__)
+        rec_dict = record.__dict__
+        for key in rec_dict:
+            if type(rec_dict[key]) not in [str,  int,  list,  dict,  float]:
+                rec_dict[key] = rec_dict[key].__dict__
+        calls.append(rec_dict)
         count += 1
         if count > 10:
             break
