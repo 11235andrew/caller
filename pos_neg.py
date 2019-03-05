48e1b8a046e7e3c7032abae3cb53996db46074de
11235andrew <sunts_andrew@mail.ru>
2019-03-05 17:31:39 +0300

    Candidats 0.

--- pos_neg.py
+++ pos_neg.py
@@ -15,8 +15,15 @@ def print_record(record):
     res = json.dumps(rec_dict,  indent=4)
     return res
     
+    
+def intersection(list1,  list2):
+    res = []
+    for el in list1:
+        if el in list2:
+            res.append(el)
+    return res
 
-def find_false_negative(vcf_file_name,  f_negative_file_name,  f_positive_file_name):
+def find_false_negative(vcf_file_name,  cand_file_name):
     try:
         vcf_file = open(vcf_file_name, 'r')
     except IOError:
@@ -24,8 +31,7 @@ def find_false_negative(vcf_file_name,  f_negative_file_name,  f_positive_file_n
         return
     
     vcf_reader = vcf.Reader(vcf_file)
-    f_neg = []
-    f_pos = []
+    pos_neg = []
     count = 0
     for record in vcf_reader:
         count += 1
@@ -33,48 +39,59 @@ def find_false_negative(vcf_file_name,  f_negative_file_name,  f_positive_file_n
             print('Record #' + str(count))
         if record.CHROM == 'chrM':
             continue
-        rec_n = {}
-        rec_n['CHROM'] = record.CHROM
-        rec_n['POS'] = record.POS
-        rec_n['owns'] = []
-        rec_p = {}
-        rec_p['CHROM'] = record.CHROM
-        rec_p['POS'] = record.POS
-        rec_p['owns'] = []
+        for fr in record.AF:
+            if record.AF > 0.01:
+                continue
+        rec = {}
+        rec['CHROM'] = record.CHROM
+        rec['POS'] = record.POS
+        rec['AF'] = ','.join(record.AF)
+        rec['QUAL'] = record.QUAL
+        rec['owns'] = []
         for sample in record.samples:
             au = sample.sample[-2]
             if au == 'u':
                 AD = sample.data.AD
                 if AD[1] > 0:
-                    rec_n['owns'].append(sample.sample)
+                    rec['owns'].append(sample.sample)
             else:
                 GT = sample.data.GT
                 if GT in ['0/1',  '0/1', '1/1']:
-                    rec_p['owns'].append(sample.sample)
-        if rec_p['owns'] != []:
-            f_pos.append(str(rec_p))
-        if rec_n['owns'] !=  []:
-            f_neg.append(str(rec_n))
-    print(str(len(f_neg)) + ' false negative records were found.')
-    print(str(len(f_pos)) + ' false positive records were found.')
+                    rec['owns'].append(sample.sample)
+        if rec['owns'] !=  []:
+            pos_neg.append(str(rec))
+#    print(str(len(f_neg)) + ' false negative records were found.')
+#    print(str(len(f_pos)) + ' false positive records were found.')
+    #print(str(len(intersection(f_neg,  f_pos))))
+    print(str(len(pos_neg)) + ' varients were found.')
     
     vcf_file.close()
     
-    try:
-        f_negative_file = open(f_negative_file_name, 'w')
-    except IOError:
-        print('File "' + f_negative_file_name + '" not found.')
-        return
-    f_negative_file.write(json.dumps(f_neg,  indent=4))
-    f_negative_file.close()
     
     try:
-        f_positive_file = open(f_positive_file_name, 'w')
+        cand_file = open(cand_file_name, 'w')
     except IOError:
-        print('File "' + f_positive_file_name + '" not found.')
+        print('File "' + cand_file_name + '" not found.')
         return
-    f_positive_file.write(json.dumps(f_pos,  indent=4))
-    f_positive_file.close()
+    cand_file.write(json.dumps(pos_neg,  indent=4))
+    cand_file.close()
+    
+#    try:
+#        f_negative_file = open(f_negative_file_name, 'w')
+#    except IOError:
+#        print('File "' + f_negative_file_name + '" not found.')
+#        return
+#    f_negative_file.write(json.dumps(f_neg,  indent=4))
+#    f_negative_file.close()
+#    
+#    try:
+#        f_positive_file = open(f_positive_file_name, 'w')
+#    except IOError:
+#        print('File "' + f_positive_file_name + '" not found.')
+#        return
+#    f_positive_file.write(json.dumps(f_pos,  indent=4))
+#    f_positive_file.close()
+    
 
 
 
@@ -84,5 +101,6 @@ if __name__ == '__main__':
     vcf_file_name = '/data/bgm/cases/bgm0187/bgm0187_wes_run2_xbrowse.vep.vcf'
     f_negative_file_name = '/home/andrey/work/Caller/caller/case_187/false_negative.json'
     f_positive_file_name = '/home/andrey/work/Caller/caller/case_187/false_positive.json'
-    find_false_negative(vcf_file_name,  f_negative_file_name,  f_positive_file_name)
+    candidats_file_name = '/home/andrey/work/Caller/caller/case_187/candidats.json'
+    find_false_negative(vcf_file_name,  candidats_file_name)
     print('Ok.')
